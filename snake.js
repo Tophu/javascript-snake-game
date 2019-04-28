ddb04120e74e02d149917f5b7875f0140f0e6f94
Phuong To <tophu95@gmail.com>
2019-04-28 23:17:51 +0100

    edited assets

--- snake.js
+++ snake.js
@@ -1,4 +1,11 @@
-/*
+/*c
 Create by Learn Web Developement
 Youtube channel : https://www.youtube.com/channel/UC8n8ftV94ZU_DJLOLtrpORA
 */
+
+const cvs = document.getElementById('snake');
+const ctx = cvs.getContext('2d');
+
+const box = 32;
+const ground = new Image();
+ground.src = "img/ground.png";
