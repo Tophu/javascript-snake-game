5f96c9b17541a4e2202e080b2fd3c0f08542f310
Phuong To <tophu95@gmail.com>
2019-04-29 00:02:18 +0100

    event listener using arrows with keycode & movement of snake

--- snake.js
+++ snake.js
@@ -16,6 +16,7 @@ ground.src = "img/ground2.png";
 const foodImg = new Image();
 foodImg.src = "img/egg2.png";
 
+// Array of Snake's body, including the head and tail
 let snake = [];
 snake[0] = {
     x: 9 * box,
@@ -31,9 +32,13 @@ let food = {
 // Initial Score like a counter
 let score = 0;
 
+// Event listner when arrow keys are pressed
 document.addEventListener('keydown', direction);
 
-// Direction of arrow keys & 'keycode' for direction
+// Declare the 'd' variable before the function, this variable will be reassigned for each arrow key pressed.
+let d;
+
+// 'keyCode' for direction, these codes are unique in JavaScript Events
 function direction(e) {
     if (e.keyCode == 37) {
         d = 'LEFT'
@@ -47,17 +52,31 @@ function direction(e) {
 }
 
 function draw() {
+    // Setting the background to the canvas
     ctx.drawImage(ground, 0, 0);
+    // Increment the Length of Snake
     for (let i = 0; i < snake.length; i++) {
+        // Add colour to the Head of Snake at it's current position
         ctx.fillStyle = (i == 0) ? "teal" : "white";
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
         ctx.strokeStyle = "darkturquiose";
         ctx.strokeRect(snake[i].x, snake[i].y, box, box);
     }
+    // Font and Score & Food Image Position
     ctx.drawImage(foodImg, food.x, food.y);
+
+    // SNAKE MOVEMENTS
+    // Old head position of snake
+    let snakeX = snake[0].x;
+    let snakeX = snake[0].y;
+
+    // Remove the tail after moving
+    snake.pop();
+
     ctx.fillStyle = "white";
     ctx.font = "36px Arial";
     ctx.fillText(score, 2 * box, 1.6 * box);
 }
 
+// Call the 'draw()' function every 100ms
 let game = setInterval(draw, 100);
