a0ffebb2bfc726e0eeea747f95471e2d654c6926
Phuong To <tophu95@gmail.com>
2019-04-28 23:48:50 +0100

    adding score to the game with comments

--- snake.js
+++ snake.js
@@ -3,11 +3,13 @@ Create by Learn Web Developement
 Youtube channel : https://www.youtube.com/channel/UC8n8ftV94ZU_DJLOLtrpORA
 */
 
+// Setting the canvas  by getting ID element from HTML
 const cvs = document.getElementById('snake');
 const ctx = cvs.getContext('2d');
 
 const box = 32;
 
+// Loading the image assets for the game
 const ground = new Image();
 ground.src = "img/ground2.png";
 
@@ -20,12 +22,29 @@ snake[0] = {
     y: 10 * box
 }
 
+// Position of food in random position every time the page is refreshed or when the snake eats the food
 let food = {
     x: Math.floor(Math.random() * 17 + 1) * box,
     y: Math.floor(Math.random() * 15 + 3) * box
 }
 
+// Initial Score like a counter
 let score = 0;
+
+document.addEventListener('keydown', direction);
+
+// Direction of arrow keys & 'keycode' for direction
+function direction(e) {
+    if (e.keyCode == 37) {
+        d = 'LEFT'
+    } else if (e.keyCode == 38) {
+        d = 'UP'
+    } else if (e.keyCode == 39) {
+        d = 'RIGHT'
+    } else if (e.keyCode == 40) {
+        d = 'DOWN'
+    }
+}
 
 function draw() {
     ctx.drawImage(ground, 0, 0);
@@ -34,8 +53,11 @@ function draw() {
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
         ctx.strokeStyle = "darkturquiose";
         ctx.strokeRect(snake[i].x, snake[i].y, box, box);
-
     }
+    ctx.drawImage(foodImg, food.x, food.y);
+    ctx.fillStyle = "white";
+    ctx.font = "36px Arial";
+    ctx.fillText(score, 2 * box, 1.6 * box);
 }
 
 let game = setInterval(draw, 100);
